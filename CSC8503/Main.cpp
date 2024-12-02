398aa077e1dbc6a0b193ee91e74c7289aa00b12d
Alfie <alfiejay@mail.com>
2024-12-02 18:42:13 +0000

    Pushdown automata

--- CSC8503/Main.cpp
+++ CSC8503/Main.cpp
@@ -33,6 +33,76 @@ using namespace CSC8503;
 
 vector<Vector3> testNodes;
 
+class PauseScreen : public PushdownState {
+	PushdownResult OnUpdate(float dt, PushdownState** newState) override {
+		if (Window::GetKeyboard()->KeyPressed(KeyCodes::U)) return PushdownResult::Pop;
+		return PushdownResult::NoChange;
+	}
+
+	void OnAwake() override {
+		std::cout << "Press U to unpause game!\n";
+	}
+};
+
+class GameScreen : public PushdownState {
+	PushdownResult OnUpdate(float dt, PushdownState** newState) override {
+		pauseReminder -= dt;
+		if (pauseReminder < 0) {
+			std::cout << "Coins mined: " << coinsMined << '\n';
+			std::cout << "Press P to pause game, or F1 to return to the main menu!\n";
+			pauseReminder += 1.0f;
+		}
+
+		if (Window::GetKeyboard()->KeyDown(KeyCodes::P)) {
+			*newState = new PauseScreen();
+			return PushdownResult::Push;
+		}
+
+		if (Window::GetKeyboard()->KeyDown(KeyCodes::F1)) {
+			std::cout << "Returning to main menu!\n";
+			return PushdownResult::Pop;
+		}
+		if (rand() % 7 == 0) coinsMined++;
+		return PushdownResult::NoChange;
+	}
+
+	void OnAwake() override {
+		std::cout << "Preparing to mine coins!\n";
+	}
+
+protected:
+	int coinsMined = 0;
+	float pauseReminder = 1;
+};
+
+class IntroScreen : public PushdownState {
+	PushdownResult OnUpdate(float dt, PushdownState **newState) override {
+		if (Window::GetKeyboard()->KeyPressed(KeyCodes::SPACE)) {
+			*newState = new GameScreen();
+			return PushdownResult::Push;
+		}
+
+		if (Window::GetKeyboard()->KeyDown(KeyCodes::ESCAPE)) {
+			return PushdownResult::Pop;
+		}
+
+		return PushdownResult::NoChange;
+	}
+
+	void OnAwake() override {
+		std::cout << "Welcome to a really awesome game!\n";
+		std::cout << "Press SPACE to begin or ESCAPE to quit!\n";
+	}
+};
+
+void TestPushdownAutomata(Window* w) {
+	PushdownMachine machine(new IntroScreen);
+	while (w->UpdateWindow()) {
+		float dt = w->GetTimer().GetTimeDeltaSeconds();
+		if (!machine.Update(dt)) return;
+	}
+}
+
 void TestPathfinding() {
 	NavigationGrid grid("TestGrid1.txt");
 
@@ -209,7 +279,7 @@ hide or show the
 */
 int main() {
 	//TestStateMachine();
-	TestBehaviourTree();
+	//TestBehaviourTree();
 
 	WindowInitialisation initInfo;
 	initInfo.width		= 1280;
@@ -220,7 +290,9 @@ int main() {
 
 	if (!w->HasInitialised()) {
 		return -1;
-	}	
+	}
+
+	TestPushdownAutomata(w);
 
 	w->ShowOSPointer(false);
 	w->LockMouseToWindow(true);
